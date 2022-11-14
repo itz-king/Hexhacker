f1f1f9724618c99055b3b6a1a93a32bf086cebb5
itz-king <115343979+itz-king@users.noreply.github.com>
2022-11-14 17:12:12 +0530

    Create auto.py

---
+++ hexhacker/modules/auto.py
@@ -0,0 +1,23 @@
+from hexhacker import *
+from telethon import events, Button
+from datetime import datetime as dt
+from hexhacker.env import *
+import asyncio
+import random
+
+statss=[]
+
+@hexhacker.on(events.NewMessage(chats=572621020,incoming=True))
+async def heck(event):
+  if len(statss) >0:
+    randm=round(random.uniform(5,10),2)
+    await asyncio.sleep(randm)
+    await hexhacker.send_message()
+    
+@hexhacker.on(events.NewMessage(outgoing=True, pattern=r"^.hexa ?(.*)"))
+async def stats(event):
+  args=event.text[7:]
+  if args=="on":
+    statss.append("True")
+  elif args=="off":
+    statss.pop()
