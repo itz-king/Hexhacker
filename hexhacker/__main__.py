7902817bf3b789fe832d519fd29f22efaa76866c
itz-king <115343979+itz-king@users.noreply.github.com>
2022-11-14 16:57:31 +0530

    Update __main__.py

--- hexhacker/__main__.py
+++ hexhacker/__main__.py
@@ -1,13 +1,11 @@
-from Asuna import *
+from hexhacker import *
 import logging
-import Asuna.modules
+import hexhacker.modules
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
 logger = logging.getLogger("__name__")
 
 print("Bot Starting....")
-asunaub.start()
-asuna.start()
-asunaub.run_until_disconnected()
-asuna.run_until_disconnected()
+hexhacker.start()
+hexhacker.run_until_disconnected()
