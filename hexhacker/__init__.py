bc2008d1756674aa97d71e7bdd7c9dc09d6ca5f0
itz-king <115343979+itz-king@users.noreply.github.com>
2022-11-14 16:56:42 +0530

    Update __init__.py

--- hexhacker/__init__.py
+++ hexhacker/__init__.py
@@ -1,10 +1,9 @@
 import os 
 from telethon.sync import TelegramClient 
 from telethon.sessions import StringSession 
-from Asuna.env import *
+from hexhacker.env import *
 
 def mention(name, userid):
   return f"[{name}](tg://user?id={userid})"
 
 asunaub = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
-asuna = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
